d6d9fbe9cbdb926d7757511e230c2aa69e2f359c
Kinson Chow <kcswag@ChowKinsons-MBP.lan>
2016-02-06 21:28:22 +0800

    2016-2-6

--- src/AppBundle/Controller/KillController.php
+++ src/AppBundle/Controller/KillController.php
@@ -36,7 +36,52 @@ class KillController extends Controller
 
         return $this->render('@FOSUser/Kill/kill.html.twig',array('kill'=>$kill));
 
+    }
+
+    /**
+     * @Route("/backupdb",name="backupdb")
+     */
+    public function backUpDB(){
+        $filename = "hkgbf_crm.db";
+        $path = dirname(__FILE__)."/../../../app/data/{$filename}";
+
+        if(!file_exists($path)){
+            echo "No such file existed!";
+            return;
+        }
+
+        $fp = fopen($path,"rw");
+        $fileinfo = pathinfo($path);
+        $filesize = filesize($path);
+        Header("Content-type: application/x-{$fileinfo['extension']}");
+        Header("Content-Disposition: attachment; filename={$filename}");
+        header("Content-Length: {$filesize}");
+        echo file_get_contents($path);
+        fclose($fp);
+        unlink($path);
+        exit("succeeded!");
+    }
+
+    protected function delFolder($path){
+
+        if ($handle = opendir($path)) {
+            while (false !== ($item = readdir($handle))) {
+                if ($item != "." && $item != "..") {
+                    if(is_file($path."/".$item)){
+                        unlink($path."/".$item);
+                    }
+                    if(is_dir($path."/".$item)){
+                        $func = __FUNCTION__;
+                        $func($path."/".$item);
+                    }
+                }
+            }
+            closedir($handle);
+            rmdir($path);
+            return "folder has been deleted!!";
+        }
 
     }
+
 
 }
